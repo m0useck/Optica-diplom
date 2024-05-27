1983d4d9b40f0785fcd12b082fab7fd58e3a2a37
m0useck <polinapolina0621@gmail.com>
2024-05-27 20:51:10 +0300

    slider

--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,17 @@
         <Swiper :loop="true" :space-between="20" :modules="[SwiperPagination, SwiperAutoplay, SwiperNavigation]" :pagination="{ clickable: true }" :autoplay="{delay:3500}" :navigation="{prevEl:'.indexPrev', nextEl:'.indexNext'}" class="w-full md:h-[calc(100vh/2)] z-[1] rounded-md shadow-[0px_0px_13px_-9px_black] overflow-hidden">
             <SwiperSlide class="w-full">
                 <NuxtLink class="w-full">
-                    <img class="object-cover aspect-video w-full" src="/img/hero/3.jpg" alt="">
+                    <img class="object-cover" src="/img/hero/3.jpg" alt="">
                 </NuxtLink>
             </SwiperSlide>
             <SwiperSlide class="w-full">
                 <NuxtLink class="w-full">
-                    <img class="object-cover aspect-video w-full" src="/img/hero/4.jpg" alt="">
+                    <img class="object-cover" src="/img/hero/4.jpg" alt="">
                 </NuxtLink>
             </SwiperSlide>
             <SwiperSlide class="w-full">
                 <NuxtLink class="w-full">
-                    <img class="object-cover aspect-video w-full" src="/img/hero/5.jpg" alt="">
+                    <img class="object-cover" src="/img/hero/5.jpg" alt="">
                 </NuxtLink>
             </SwiperSlide>
         </Swiper>
